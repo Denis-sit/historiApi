0f3ae221baab1a5a936362abe46a01aadda8699a
Denis-sit <specden990@mail.ru>
2021-08-26 12:14:29 +0500

    Исправлен путь подключения файлов

--- js/script.js
+++ js/script.js
@@ -4,25 +4,38 @@ document.addEventListener('DOMContentLoaded', function(){
     const classArray= ['button-nav__registration-selected', 'button-nav__visibility-and-access-selected', 'button-nav__beginning-of-work-selected'];
     const questionButton = document.querySelectorAll('.question-button'),
           answerToTheQuestion = document.querySelectorAll('.block-answer');
-    
-    btsNav.forEach((item, k) =>{
-        item.addEventListener('click', function (e) {
-            e.preventDefault();
-               btsNav.forEach((bts, i) =>{
-                   bts.classList.remove(classArray[i]);
-                   blockContent[i].classList.add('hide');
-               })    
-               item.classList.add(classArray[k]);
-               blockContent[k].classList.remove('hide');
-               closingBlocksWithInformation(answerToTheQuestion);
-         });
-    });
+    const arrayHistori = ['=kak-vojti-v-konstruktor', '=kak-sdelat-tak-chtoby-moi-kollegi-tozhe-mogli-rabotat-s-konstruktorom' , 
+                         '=mogut-li-kollegi-videt-moi-oprosy-i-oprosy-drug-druga', '=kak-predostavit-dostup-k-moemu-oprosu-kollege',
+                         '=kak-nachat-rabotu-s-konstruktorom', '=kak-pobystree-osvoit-konstruktor'];
+    const arrayHistori2 =['registration?answer', 'access?answer', 'start_work?answer'];
+          
+         
+    history.pushState(null, null,  '/help');
+          window.addEventListener('popstate', function(e) {s
+            
+        });
+        window.history.back()
+        btsNav.forEach((item, k) =>{
+            item.addEventListener('click', function (e) {
+                e.preventDefault();
+                   btsNav.forEach((bts, i) =>{
+                       bts.classList.remove(classArray[i]);
+                       blockContent[i].classList.add('hide');
+                   })    
+                   item.classList.add(classArray[k]);
+                   blockContent[k].classList.remove('hide');
+                   closingBlocksWithInformation(answerToTheQuestion);
+                   history.pushState('', '',  arrayHistori2[i]);
+             });
+        });
+        
     
     questionButton.forEach((item, i) =>{
         item.addEventListener('click', function(e) {
             e.preventDefault();
             answerToTheQuestion[i].classList.toggle('hide');
             item.classList.toggle('open');
+            history.pushState('', '',  arrayHistori[i]);
         })
     })
     
